974477ae96cf0f4f8365336fccc221251fad8f8a
Magno Costa <magnocp123@gmail.com>
2020-04-02 17:17:52 -0300

    casa criativa

---
+++ server.js
@@ -0,0 +1,117 @@
+const express = require("express");
+
+const server = express();
+
+const db = require("./db")
+
+/*const ideas = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title: "Cursos de Programação",
+        category: "Estudo",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias voluptatem amet veritatis ullam, numquam voluptas repudiandae consectetur sequi consequatur. Ad a quibusdam explicabo animi sed quas natus ipsam perferendis at.",
+        url: "#"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title: "Exercicios",
+        category: "Saúde",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias voluptatem amet veritatis ullam, numquam voluptas repudiandae consectetur sequi consequatur. Ad a quibusdam explicabo animi sed quas natus ipsam perferendis at.",
+        url: "#"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title: "Meditação",
+        category: "Mentalidade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias voluptatem amet veritatis ullam, numquam voluptas repudiandae consectetur sequi consequatur. Ad a quibusdam explicabo animi sed quas natus ipsam perferendis at.",
+        url: "#"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/1776/1776597.svg",
+        title: "Tocar um instrumento",
+        category: "Diversão",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias voluptatem amet veritatis ullam, numquam voluptas repudiandae consectetur sequi consequatur. Ad a quibusdam explicabo animi sed quas natus ipsam perferendis at.",
+        url: "#"
+    },
+]*/
+
+// arquivos estaticos
+server.use(express.static('public'))
+
+// habilitar uso do req.bory
+server.use(express.urlencoded({extended: true}))
+
+//nunjucks
+const nunjucks = require("nunjucks")
+nunjucks.configure("views",{
+    express: server, 
+    noCache: true,   
+})
+
+
+// rotas
+server.get("/", function (req, res){
+
+    db.all(`SELECT * FROM ideas`, function (err, rows){
+        if (err) return console.log(err)
+
+        const reversedIdeas = [...rows].reverse()
+
+        let lastIdeas = []
+
+        for( let idea of reversedIdeas){
+            if(lastIdeas.length < 2){
+                lastIdeas.push(idea)
+            }
+        }   
+
+        return res.render("index.html", { ideas: lastIdeas })
+        })
+})
+
+server.get("/ideias", function (req, res){
+
+    db.all(`SELECT * FROM ideas`, function (err, rows){
+        if (err){
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        } 
+
+        const reversedIdeas = [...rows].reverse()
+        return res.render("ideias.html", { ideas: reversedIdeas })
+
+    })   
+})
+
+server.post("/", function(req, res){
+    // Inserir dados
+    const query =  (`    
+            INSERT INTO ideas(
+                image,
+                title, 
+                category,
+                description,
+                link
+            )VALUES(?, ?, ?, ?, ?);
+    `)
+
+    const values = [
+        req.body.image, 
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link        
+    ]
+
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no bano de dados!")
+        }
+        return res.redirect("/ideias")        
+    })
+    
+})
+
+
+server.listen(3000);
